caacdaa31cf7e7439f8ccb4995b3cc7175de39ec
korenrolle <korenrolle21@gmail.com>
2023-01-31 11:17:03 -0800

    updated schema

--- src/Pages/Music/Feed.jsx
+++ src/Pages/Music/Feed.jsx
@@ -1,18 +1,25 @@
 import React, { useState, useEffect } from "react";
 import CreatePost from "../../Components/Post/CreatePost";
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 
 function Feed() {
   // State for the list of posts
   const [posts, setPosts] = useState([]);
   
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
 
   // Function to fetch the list of posts from the backend API
   async function fetchPosts() {
-    const res = await fetch("http://localhost:4000/post");
-    const data = await res.json();
-    setPosts(data);
+    try {
+      const res = await fetch("http://localhost:4000/post");
+      if (!res.ok) {
+        throw new Error("Failed to fetch posts");
+      }
+      const data = await res.json();
+      setPosts(data);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   useEffect(() => {
@@ -21,34 +28,46 @@ function Feed() {
 
   // Function to handle a comment submission for a post
   async function handleCommentSubmit(postId, comment) {
-    // Send the comment to the backend API
-    await fetch(`/api/posts/${postId}/comments`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ comment }),
-    });
-
-    // Refresh the list of posts
-    fetchPosts();
+    try {
+      // Send the comment to the backend API
+      const res = await fetch(`/posts/${postId}/comments`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ comment }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to submit comment");
+      }
+      // Refresh the list of posts
+      fetchPosts();
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   // Function to handle delete a post
   async function handleDeletePost(postId) {
-    // Send the delete request to the backend API
-    await fetch(`/api/posts/${postId}`, {
-      method: "DELETE",
-    });
-
-    // Refresh the list of posts
-    fetchPosts();
+    try {
+      // Send the delete request to the backend API
+      const res = await fetch(`/posts/${postId}`, {
+        method: "DELETE",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to delete post");
+      }
+      // Refresh the list of posts
+      fetchPosts();
+    } catch (error) {
+      console.error(error);
+    }
   }
+
   return (
     <div>
       <CreatePost fetchPosts={fetchPosts} />
-      {posts.map((post) => {
-        return (
+      {posts.map((post) => (
         <div key={post.id}>
           <img src={post.image} alt={post.title} />
           <h3>{post.title}</h3>
@@ -59,9 +78,9 @@ function Feed() {
           </form>
           <button onClick={() => handleDeletePost(post.id)}>Delete</button>
         </div>
-)})}
+      ))}
     </div>
   );
+}
 
-      }
 export default Feed;
--- src/components/Post/CreatePost.jsx
+++ src/components/Post/CreatePost.jsx
@@ -3,12 +3,12 @@ import { useState, useEffect } from 'react'
 
 // CreatePost component
 function CreatePost() {
-    const [post, setPost] = useState(null)
-    const [createForm, setCreateForm] = useState("")
+    const [post, setPost] = useState({})
+    const [createForm, setCreateForm] = useState({ text: '', comments: '' })
 
     const navigate = useNavigate()
 
-    const URL = `http://localhost:3000/create`
+    const URL = `http://localhost:4000/post/`
 
     // handle input changes in the form
     const handleChange = (e) => setCreateForm({ ...createForm, [e.target.name]: e.target.value })
@@ -28,7 +28,9 @@ function CreatePost() {
 
             // update the post state with the newly created post
             setPost(newPost)
-            setCreateForm(newPost)
+            console.log(post)
+            setCreateForm({ text: '', comments: '' })
+            console.log(createForm)
 
         } catch (err) {
             console.log(err)
@@ -40,17 +42,31 @@ function CreatePost() {
     const renderForm = () => (
         <form onSubmit={createPost}>
             <input
-                type="text"
-                value={createForm.title}
-                name="text"
+                name="name"
+                type="name"
+                value={createForm.name}
                 placeholder="Title"
                 onChange={handleChange}
             />
             <input
-                type="text"
-                value={createForm.description}
-                name="comments"
+                type="buffer" alt=""
+                value={createForm.image}
+                name="image"
                 placeholder="Description"
+                onChange={handleChange}
+            />
+            <input
+                type="title"
+                value={createForm.title}
+                name="title"
+                placeholder="Description"
+                onChange={handleChange}
+            />
+            <input
+                type="title"
+                value={createForm.mp3}
+                name="mp3"
+                placeholder="mp3"
                 onChange={handleChange}
             />
             <input type="submit" value="Create Post" />
