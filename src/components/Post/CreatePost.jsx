90c588a3729b7f6977e4aa706e5273bb4edbd719
korenrolle <korenrolle21@gmail.com>
2023-01-31 14:51:57 -0800

    fixed body parcer error

--- src/components/Post/CreatePost.jsx
+++ src/components/Post/CreatePost.jsx
@@ -1,84 +1,181 @@
-import { useParams, useNavigate } from 'react-router-dom'
+// import './Post.css'
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
 
-// CreatePost component
-function CreatePost() {
-    const [post, setPost] = useState({})
-    const [createForm, setCreateForm] = useState({ text: '', comments: '' })
+const Post = (props) => {
+    // define our state variable - []
+    // react state
+    const [post, setPost] = useState([])
+    const [newForm, setNewForm] = useState({
+        name: "",
+        image: "",
+        title: "",
+    })
+    // fetch endpoint
+    const BASE_URL = "http://localhost:4000/post"
 
-    const navigate = useNavigate()
+    // create some local state for tracking post input (user) ++
+    // link this state to a controlled form (post) ++
+    // handlers (change ++ / submit )
+    // submit event will make a post request from our current comp.
 
-    const URL = `http://localhost:4000/post/`
-
-    // handle input changes in the form
-    const handleChange = (e) => setCreateForm({ ...createForm, [e.target.name]: e.target.value })
-
-    // function to create a new post
-    const createPost = async (e) => {
-        e.preventDefault()
+    const getPost = async () => {
         try {
-            const options = {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(createForm)
-            }
-            // send the post request to the server
-            const response = await fetch(URL, options)
-            const newPost = await response.json()
-
-            // update the post state with the newly created post
-            setPost(newPost)
-            console.log(post)
-            setCreateForm({ text: '', comments: '' })
-            console.log(createForm)
-
+            const response = await fetch(BASE_URL)
+            // fetch grabs the data from API - (mongo)
+            const allPost = await response.json()
+            // assuming no errors - translate to JS 
+            // console.log(allPost)
+            setPost(allPost)
+            // store that data (from api) in react state
         } catch (err) {
             console.log(err)
-            // navigate(URL)
         }
     }
 
-    // render the form for creating a new post
-    const renderForm = () => (
-        <form onSubmit={createPost}>
-            <input
-                name="name"
-                type="name"
-                value={createForm.name}
-                placeholder="Title"
-                onChange={handleChange}
-            />
-            <input
-                type="buffer" alt=""
-                value={createForm.image}
-                name="image"
-                placeholder="Description"
-                onChange={handleChange}
-            />
-            <input
-                type="title"
-                value={createForm.title}
-                name="title"
-                placeholder="Description"
-                onChange={handleChange}
-            />
-            <input
-                type="title"
-                value={createForm.mp3}
-                name="mp3"
-                placeholder="mp3"
-                onChange={handleChange}
-            />
-            <input type="submit" value="Create Post" />
-        </form> 
-    )
+    const handleChange = (e) => {
+        // console.log(newForm)
+        const userInput = { ...newForm }
+        userInput[e.target.name] = e.target.value
+        setNewForm(userInput)
+    }
 
+    const handleSubmit = async (e) => {
+        // 0. prevent default (event object method)
+        e.preventDefault()
+        // 1. capturing our local state
+        const currentState = { ...newForm }
+        // check any fields for property data types / truthy value (function call - stretch)
+        try {
+            const requestOptions = {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(currentState)
+            }
+            // 2. specify request method , headers, Content-Type
+            // 3. make fetch to BE - sending data (requestOptions)
+
+            // 3a fetch sends the data to API - (mongo)
+            const response = await fetch(BASE_URL, requestOptions)
+            // 4. check our response - 
+            // 5. parse the data from the response into JS (from JSON) 
+            const createdSinglePost = await response.json()
+            console.log(createdSinglePost)
+            // update local state with response (json from be)
+            setPost([...post, createdSinglePost])
+            console.log(post)
+            // reset newForm state so that our form empties out
+            setNewForm({
+                name: "",
+                image: "",
+                title: "",
+            })
+
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    // const loaded = () => {
+    //     return (<>
+    //         <section className="post-list">
+    //             {post?.map((singlePost) => {
+    //                 return (
+    //                     <Link key={singlePost._id} to={`/post/${singlePost._id}`}>
+    //                         <div className="singlePost-card">
+    //                             {/* React optimization / difference */}
+    //                             <h1>{singlePost.name}</h1>
+    //                             <img alt=""src={singlePost.image} />
+    //                             <h3>{singlePost.title}</h3>
+    //                         </div>
+    //                     </Link>
+    //                 )
+    //             })
+    //             }
+    //         </section>
+    //     </>
+    //     )
+    // }
+
+    // const loading = () => (
+    //     <section className="post-list">
+    //         <h1>
+    //             Loading...
+    //             <span>
+    //                 <img alt=""
+    //                     className="spinner"
+    //                     src="https://freesvg.org/img/1544764567.png"
+    //                 />
+    //             </span>
+    //         </h1>
+    //     </section>
+    // );
+
+    useEffect(() => {
+        getPost()
+    }, [])
+
+
+   
+    
+    // useEffect takes two arguments -> runs function upon component mount
+    // react mount -> 
+   
     return (
         <div>
-            <h1>Create Post</h1>
-            {renderForm()}
-        </div>
-    );
+    <h1>Create Post</h1>
+    <section className='postForm'>
+            <h2>Create A New Post</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor='name'>
+                        Name:
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            placeholder="post name"
+                            value={newForm.name}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor='image'>
+                        Image:
+                        <input
+                            type="text"
+                            id="image"
+                            name="image"
+                            placeholder="post image"
+                            value={newForm.image}
+                            onChange={handleChange}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label htmlFor='title'>
+                        Title:
+                        <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            placeholder="post title"
+                            value={newForm.title}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <br />
+                    <input className='button' type="submit" value="Create a Post" />
+                </div>
+            </form>
+        </section>
+        {/* {post && post.length ? loaded() : loading()} */}
+</div>
+    )
+
 }
 
-export default CreatePost;
+export default Post
